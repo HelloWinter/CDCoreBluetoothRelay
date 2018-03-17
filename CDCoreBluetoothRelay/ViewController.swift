0e8c71dc7dcd3e20b048a5f31cb9a450a40037b3
dong cheng <>
2018-03-17 19:56:15 +0800

    初始化蓝牙工程

---
+++ CDCoreBluetoothRelay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CDCoreBluetoothRelay
+//
+//  Created by dong cheng on 2018/3/17.
+//  Copyright © 2018年 dong cheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
